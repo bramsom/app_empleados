133e9d0cd911e8f19fe07559c70f182c932dd033
bramsom <brayansmartinez0607@gmail.com>
2025-07-04 16:48:44 -0500

    envio login con avance del 98% funcional con estilizacion segun prototipado

--- views/apprentice_panel.py
+++ views/apprentice_panel.py
@@ -1,40 +1,88 @@
 import customtkinter as ctk
-from tkinter import Canvas
 from PIL import Image
 
-ventana = ctk.CTk()
-ventana.geometry("600x500")
-ventana.resizable(False, False)
+class MenuDesplegable(ctk.CTkFrame):
+    def __init__(self, master, usuario):
+        super().__init__(master, width=20, corner_radius=20, fg_color="#2F2F2F")
+        self.pack(side="left", fill="y")
+        self.usuario = usuario
+        self.expanded = False
+        self._hover_id=None
+        self.hover_count = 0  # Rastrea el número de widgets donde el mouse está encima
 
-# Frame Izquierdo
-frame_izquierdo = ctk.CTkFrame(ventana, width=300, corner_radius=0)
-frame_izquierdo.pack(side="left", fill="y")
+        # Vincula eventos al propio frame
+        self.bind("<Enter>", self._hover_in, add="+")
+        self.bind("<Leave>", self._hover_out, add="+")
 
-# Canvas para dibujar decoraciones
-canvas = Canvas(frame_izquierdo, width=300, height=500, bg="#FFEFEF", highlightthickness=0)
-canvas.pack()
+        self.botones = []
 
+        # Ejemplo para botón de Inicio
+        icono_inicio = ctk.CTkImage(Image.open("C:/Users/Usuario/Documents/proyectos python/app_empleados/images/logo.png"), size=(24, 24))
 
-canvas.create_polygon(80, 0, 140, 0, 260, 120, 230, 150, fill="#D2D2D2", outline="")# Dibujar bloques (líneas o rectángulos inclinados)
-canvas.create_polygon(200, 80, 240, 40, 300, 100, 300, 180, fill="#888888", outline="")
-canvas.create_polygon(240, 0, 300, 0, 300, 60, 300, 60, fill="#D2D2D2", outline="")
-canvas.create_polygon(233, 0, 164, 0, 255, 92, 290, 57, fill="#D12B1B", outline="")
-canvas.create_polygon(200, 0, 201, 0, 260, 60, 260, 60, fill="#FCFCFC", outline="")
-canvas.create_polygon(109, 0, 109, 0, 169, 60, 170, 60, fill="#FCFCFC", outline="")
+        self.boton_inicio = ctk.CTkButton(
+            self,
+            text="Inicio",
+            image=icono_inicio,
+            compound="left",
+            anchor="w",
+            fg_color="transparent",
+            hover_color="#3F3F3F",
+            command=self.ir_inicio
+        )
+        self.boton_inicio.pack(pady=5, padx=10, fill="x")
 
-
-canvas.create_polygon(220, 500, 280, 500, 80, 300, 50, 330, fill="#D2D2D2", outline="")# Dibujar bloques (líneas o rectángulos inclinados)
-canvas.create_polygon(0, 250, 0, 330, 90, 420, 130, 380, fill="#888888", outline="")
-canvas.create_polygon(190, 500, 110, 500, 20, 410, 60, 370, fill="#D12B1B", outline="")
-canvas.create_polygon(151, 500, 150, 500, 50, 400, 50, 400, fill="#FCFCFC", outline="")
-canvas.create_polygon(250,500, 251,500, 150, 400, 150, 400, fill="#FCFCFC", outline="")
-
-
-# Logo encima
-logo_img = ctk.CTkImage(Image.open("C:/Users/Usuario/Documents/proyectos python/app_empleados/images/logo.png"), size=(140, 150))
-logo_label = ctk.CTkLabel(frame_izquierdo, image=logo_img, text="")
-logo_label.place(relx=0.55, rely=0.45, anchor="center")
-
-ventana.mainloop()
+        # Guardamos el botón y su texto original
+        self.botones.append((self.boton_inicio, "Inicio"))
 
 
+    def crear_boton(self, texto, comando):
+        boton = ctk.CTkButton(
+            self, text=texto, anchor="w",
+            fg_color="transparent", hover_color="#3F3F3F",
+            command=comando
+        )
+        boton.pack(pady=10, padx=10, fill="x")
+        boton.bind("<Enter>", self._hover_in, add="+")
+        boton.bind("<Leave>", self._hover_out, add="+")
+        return boton
+        
+        #self._hover_id = None  # Variable para controlar cancelación de temporizador
+
+    def _hover_in(self, event=None):
+        if self._hover_id:
+            self.after_cancel(self._hover_id)
+            self._hover_id = None
+        self.expandir_menu()
+
+    def _hover_out(self, event=None):
+        self._hover_id = self.after(300, self.verificar_mouse_fuera)
+
+    def verificar_mouse_fuera(self):
+        x, y = self.winfo_pointerxy()
+        widget_bajo_cursor = self.winfo_containing(x, y)
+        if widget_bajo_cursor not in self.winfo_children() + [self]:
+            self.colapsar_menu()
+
+    def expandir_menu(self):
+        self.configure(width=200)  # Ajusta el ancho del menú
+        for boton, texto in self.botones:
+            boton.configure(text=texto, anchor="w", compound="left")
+
+    def colapsar_menu(self):
+        self.configure(width=60)
+        for boton, _ in self.botones:
+            boton.configure(text="", anchor="center", compound="left")
+
+    # Funciones simuladas
+    def ir_inicio(self):
+        print("Ir a Inicio")
+
+    def ir_usuarios(self):
+        print("Ir a Usuarios")
+
+# Uso
+if __name__ == "__main__":
+    app = ctk.CTk()
+    app.geometry("700x500")
+    menu = MenuDesplegable(app, usuario="Laura Pérez")
+    app.mainloop()
